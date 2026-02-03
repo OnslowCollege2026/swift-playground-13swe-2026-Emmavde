fa4364701e7360d5b4151b0dacc1c231364887bc
Emmavde <142274999+Emmavde@users.noreply.github.com>
2026-02-03 23:57:30 +0000

    Completed first 3 parts

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,46 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+
+        // These are the lunch costs for Monday to Friday.
+        let lunches: [Double] = [6.50, 8.00, 5.75, 9.20, 7.10]
+        // Use a for loop to print the lunch cost for each day.
+        for (index, cost) in lunches.enumerated() {
+            print("Day \(index + 1): $\(cost)")
+        }
     }
 }
+
+
+let budget: Double = 35.00
+
+///Total Cost Function
+///
+/// This should return the total cost of all lunches.
+func totalCost(prices: [Double]) -> Double {
+    var sum: Double = 0.0
+    for cost in prices {
+        sum += cost
+    }
+    return sum
+}
+
+/// Budget Check Function
+/// 
+/// This should return true if the student spent more than the budget.
+func isOverBudget(total: Double, budget: Double) -> Bool {
+    return total > budget
+}
+
+/// Average Cost Function
+/// 
+/// This should return the average lunch cost per day.
+func averageCost(prices: [Double]) -> Double {
+
+    var sum: Double = 0
+    for price in prices {
+        sum += price
+    }
+    return sum / Double(prices.count)
+}
+
