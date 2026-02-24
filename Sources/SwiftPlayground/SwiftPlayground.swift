cac0dd73a1c8242da0213478ee91e325bb734aed
Emmavde <142274999+Emmavde@users.noreply.github.com>
2026-02-24 00:46:32 +0000

    Finsihed task E - need to do extension task

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -68,8 +68,8 @@ struct Timer {
 // In comments, explain why addItem() and shippingMessage() are instance behaviour, while freeShippingThreshold and qualifiesForFreeShipping are type-level behaviour.
 
 struct Cart {
-    var itemsCount = 0
-    let freeShippingThreshold = 5
+    var itemsCount: Int = 0
+    let freeShippingThreshold: Int = 5
 
     mutating func addItem() {
         itemsCount += 1
@@ -81,22 +81,27 @@ struct Cart {
         } else { return "Shipping applies" }
     }
 
-    let qualifiesForFreeShipping(count: Int) -> Bool {
-        itemsCount >= freeShippingThreshold 
+    func qualifiesForFreeShipping(count: Int) -> Bool {
+        count >= freeShippingThreshold 
     }
-
-
 }
 
 
 // Task E: method to computed property
-// Create a struct called Badge with properties name and level.
-// Add an instance method label() -> String that returns text like "Ranger - Level 4".
-// Create at least one Badge instance and print the method result.
-// Convert label() into a computed property label: String.
-// Print the new computed property and confirm the output is the same.
+struct Badge {
+    let name: String
+    let level: Int
+
+    var label: String {
+        return "\(name) - level \(level)"
+    }
+
+}
+
+
 // Extension for Super Players!
 // Take one extra step to strengthen your design choices with computed properties. Add at least one computed property to each of Tasks A through D, or convert one existing method from any task into a computed property and explain why that change improves readability.
+
 @main
 struct SwiftPlayground {
     static func main() {
@@ -135,5 +140,28 @@ struct SwiftPlayground {
         }
 
         tenSecTimer.reset()
+
+        // Task D: mixed design decision
+        // Create one Cart instance and call addItem() in a loop.
+        // After each add, print both cart.itemsCount and cart.shippingMessage().
+        // In comments, explain why addItem() and shippingMessage() are instance behaviour, while freeShippingThreshold and qualifiesForFreeShipping are type-level behaviour.
+
+        var cart: Cart = Cart()
+
+        while cart.itemsCount <= 10 {
+            cart.addItem()
+            print("\nItems in the cart: \(cart.itemsCount)")
+            print("Shipping status: \(cart.shippingMessage())")
+        }
+
+        // Task E: method to computed property
+        // Create at least one Badge instance and print the method result.
+        // Convert label() into a computed property label: String.
+        // Print the new computed property and confirm the output is the same.
+
+        let badge: Badge = Badge(name: "Warrior", level: 10)
+        print()
+        print(badge.label)
+
     }
 }
