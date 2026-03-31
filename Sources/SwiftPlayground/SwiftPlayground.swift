dc7bebec76534fda73018feb5d75cd21229c6003
Emmavde <142274999+Emmavde@users.noreply.github.com>
2026-03-31 23:25:44 +0000

    did

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,11 +1,17 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import Foundation
+import GRDB
 
 @main
 struct SwiftPlayground {
     static func main() {
-
-        
+        let dbPath = "./Sources/SwiftPlayground/cafe.db"
+        guard let dbQueue = try? DatabaseQueue(path: dbPath) else {
+            fatalError("Could not open database.")
+        }
     }
 }
+
+
+struct Purchaser: Idenifiable, 
