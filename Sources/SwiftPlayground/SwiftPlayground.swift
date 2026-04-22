4d4d003d813d215035a6eefb68fc079f25e84bc6
Emmavde <142274999+Emmavde@users.noreply.github.com>
2026-04-22 04:46:04 +0000

    Did stuff

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -8,15 +8,47 @@ import GRDB
 
 /// A Borrower who takes out loans. 
 struct Borrower: Identifiable, Codable, FetchableRecord, PersistableRecord {
-    /// The Borrower ID
+    /// The Borrower ID.
     let id: Int
 
-    /// The Borrower's name
+    /// The Borrower's name.
     let name: String
 
     /// The number of loans the borrower currently has out/unreturned.
     let currentLoans: Int
 
+    enum CodingKeys: String, CodingKey {
+        case id = "Borrower ID"
+        case name = "Name"
+        case currentLoans = "Current Loans"
+    }
+}
+
+/// A book that can be loaned. 
+struct Book: Identifiable, Codable, FetchableRecord, PersistableRecord {
+    /// The Book ID.
+    let id: Int
+
+    /// The Books's title.
+    let title: String
+
+    /// The Book's author.
+    let author: String
+
+    /// Whether the book is avialable or not. 0 = false (not available), 1 = true (available)
+
+    // var available: Int {
+    //     // return Loans.returned.at book ID !contains(0)
+    // }
+    
+
+
+    enum CodingKeys: String, CodingKey {
+        case id = "Book ID"
+        case title = "Title"
+        case author = "Author"
+        // case available = "Available"
+    }
 }
 
 @main
