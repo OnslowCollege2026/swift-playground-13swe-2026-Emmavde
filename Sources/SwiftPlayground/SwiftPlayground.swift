6070a998901c39993a70e21698277b09ba70a57e
Emmavde <142274999+Emmavde@users.noreply.github.com>
2026-04-24 00:37:58 +0000

    did 2 more function

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -7,7 +7,7 @@ import Foundation
 import GRDB
 
 /// A Borrower who takes out loans.
-struct Borrower: Identifiable {
+struct Borrower: Identifiable, CustomStringConvertible{
     static let databaseTableName = "Borrower"
 
     /// The Borrower ID.
@@ -15,6 +15,10 @@ struct Borrower: Identifiable {
 
     /// The Borrower's name.
     var name: String
+
+    var description: String {
+        "ID; \(id) | Name: \(name) "
+    }
 }
 
 /// A book that can be loaned.
@@ -28,6 +32,9 @@ struct Book: Identifiable, CustomStringConvertible {
 
     /// The Book's author.
     let author: String
+
+    // Whether or not the book stilll exists in the libraries catalouge.
+    var exists: Bool
 
     var description: String {
         "ID: \(id) | Title: \(title) | Author: \(author)"
@@ -92,7 +99,7 @@ func placeHolder() {
 }
 
 let actionOptions: [menuOption] = [
-    menuOption(optionNumber: 1, description: "View books", action: placeHolder()),
+    menuOption(optionNumber: 1, description: "View books", action:()),
     menuOption(optionNumber: 2, description: "Add book", action: ()),
     menuOption(optionNumber: 3, description: "Delete book", action: ()),
     menuOption(optionNumber: 4, description: "Borrow book", action: ()),
@@ -104,16 +111,16 @@ let actionOptions: [menuOption] = [
 
 // Some books to add to the library for testing.
 let preSetBooks: [Book] = [
-    Book(id: 1, title: "1984", author: "George Orwell"),
-    Book(id: 2, title: "The Ultimate Guide to Swordfish", author: "Victoria Chew"),
-    Book(id: 3, title: "The Hobbit", author: "J.R.R. Tolkien"),
-    Book(id: 4, title: "Pride and Prejudice", author: "Jane Austen"),
-    Book(id: 5, title: "A Study in Scarlet", author: "Sir Arthur Conan Doyle"),
-    Book(id: 6, title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams"),
-    Book(id: 7, title: "The Lord of the Rings", author: "J.R.R. Tolkien"),
-    Book(id: 8, title: "Animal Farm", author: "George Orwell"),
-    Book(id: 9, title: "Holes", author: "Louis Sachar"),
-    Book(id: 10, title: "The 3 Body Problem", author: "Cixin Liu"),
+    Book(id: 1, title: "1984", author: "George Orwell", exists: true),
+    Book(id: 2, title: "The Ultimate Guide to Swordfish", author: "Victoria Chew", exists: true),
+    Book(id: 3, title: "The Hobbit", author: "J.R.R. Tolkien", exists: true),
+    Book(id: 4, title: "Pride and Prejudice", author: "Jane Austen", exists: true),
+    Book(id: 5, title: "A Study in Scarlet", author: "Sir Arthur Conan Doyle", exists: true),
+    Book(id: 6, title: "The Hitchhiker's Guide to the Galaxy", author: "Douglas Adams", exists: true),
+    Book(id: 7, title: "The Lord of the Rings", author: "J.R.R. Tolkien", exists: true),
+    Book(id: 8, title: "Animal Farm", author: "George Orwell", exists: true),
+    Book(id: 9, title: "Holes", author: "Louis Sachar", exists: true),
+    Book(id: 10, title: "The 3 Body Problem", author: "Cixin Liu", exists: true),
 ]
 
 // SOme borrower data to add to the library for testing.
@@ -157,42 +164,70 @@ func viewBooks(books: [Book], loans: [Loan]) {
         """)
 
     var looping: Bool = true
+    var filteredBooks: [Book] = []
+
     while looping {
-        if let optionInput: String = input(forString: "Please enter the option letter: ") {
-
-            for book in books {
-                var availability: String = ""
-
-                if book.isAvailable(book: book, loans: loans) {
-                    availability = "Available"
-                } else {
-                    availability = "Not Available"
-                }
-
-                if optionInput.lowercased() == "a" {
-                    
-                    looping = false
-                    print("\(book) - \(availability)")
-                } 
-
-                if optionInput.lowercased() == "b"
-                && books.filter({$0.isAvailable(book: book, loans: loans)}).contains(where: {$0.id == book.id}) {
-
-                    looping = false
-                    print("book")
-                } 
+        guard let optionInput: String = input(forString: "Please enter the option letter: ") else {
+            print("Invalid")
+            continue
+        } 
 
 
+        if optionInput == "a" {
+            looping = false
+            filteredBooks = books.filter({$0.exists})
+        } else if optionInput == "b" {
+            looping = false
+            filteredBooks = books.filter({$0.exists && $0.isAvailable(book: $0, loans: loans)})
+        } else {
+            print("Invalid. Enter 'a' or 'b'.")
+        }
+    }
 
-            }
+    print(
+        """
+        \nBook list:
+        ----------------
+        """)
+
+    for book in filteredBooks {
+        var availability: String = "Not available"
+        if book.isAvailable(book: book, loans: loans) {
+            availability = "Available"
+        }
+        print("\(book) | \(availability)")
         }
 
     }
-        
+
+func viewBorrowers(borrowers: [Borrower]) {
+    for borrower in borrowers {
+        print(borrower)
+    }
 }
-    
 
+func addBooks(to books: inout [Book]) {
+    print("""
+    Add a book:
+    ------------------
+    """)
 
+    let id = (books.map{$0.id}.max() ?? 0) + 1
+    if let title = input(forString: "Enter book title: ") {
+        if let author = input(forString: "Enter Author's name: ") {
+            let newBook: Book = Book(id: id, title: title, author: author, exists: true)
+            books.append(newBook)
+            print("\(newBook) was added")
+        }
+    }
+}
+
+func removeBook(from books: inout [Book]) {
+    print("""
+    Remove a book: (This will not remove the book from loan history.)
+
+    """)
+}
 @main
 struct SwiftPlayground {
     static func main() {
@@ -212,7 +247,7 @@ struct SwiftPlayground {
 
             guard
                 let optionInput: String = input(
-                    forString: "\nEnter option number, or 'done' to finish.): ")
+                    forString: "\nEnter option number, or 'done' to finish: ")
             else {
                 print("Invalid input")
                 continue
@@ -229,11 +264,11 @@ struct SwiftPlayground {
                     // }
                     switch optionNumber {
                     case 1: viewBooks(books: books, loans: loans)
-                    case 2: placeHolder()
+                    case 2: addBooks(to: &books)
                     case 3: placeHolder()
                     case 4: placeHolder()
                     case 5: placeHolder()
-                    case 6: placeHolder()
+                    case 6: viewBorrowers(borrowers: borrowers)
                     case 7: placeHolder()
                     case 8: placeHolder()
                     default: print("Invalid. Please enter a number or 'done'.")
